1fb74d6659488a5542854f57f5b15033d8e1af42
Ben Boyle <benjamins.boyle@gmail.com>
2014-05-14 18:51:59 +1000

    implement functionality
    - use pink to differentiate the minus button
    - use @pattern to enforce (positive) integer
    - calculate and display correct result

--- src/addSubtract.js
+++ src/addSubtract.js
@@ -10,10 +10,24 @@ $(function() {
 
 	// manage calculator submissions
 	$( 'form' )
+
 	.on( 'submit', function( event ) {
 		event.preventDefault();
-		$( '#result' ).text( button.value ).removeClass( 'hide' );
+
+		// get inputs
+		// note: yep, relying on HTML5 form validation to ensure these are integers
+		var x = parseInt( $( '#x' ).val(), 10 );
+		var y = parseInt( $( '#y' ).val(), 10 );
+
+		var z = button.value === 'plus' ? x + y : x - y;
+
+		$( '#result' )
+		.text( x + ' ' + button.value + ' ' + y + ' is ' + z )
+		.removeClass( 'hide plus minus' )
+		.addClass( button.value );
+
 	})
+
 	.on( 'click', 'button', function() {
 		button = this;
 	});
